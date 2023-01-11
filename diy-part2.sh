5bbe70de4549d9cab38b09fc19050ecffa24ab02
MJ <MJ>
2023-01-12 02:59:15 +0800

    2.59

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,6 +10,10 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
+LUCI_DIR='package/mj'
+PACK_DIR='package/mj'
+ln -s ../feeds/luci/luci.mk package/luci.mk
+
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
 
@@ -18,29 +22,32 @@ sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_genera
 #修正连接数（by ベ七秒鱼ベ）
 sed -i '/customized in this file/a net.netfilter.nf_conntrack_max=165535' package/base-files/files/etc/sysctl.conf
 
-# themes添加（svn co 命令意思：指定版本如https://github）
-git clone https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom package/luci-theme-infinityfreedom
-git clone https://github.com/Leo-Jo-My/luci-theme-opentomcat.git package/luci-theme-opentomcat
-git clone https://github.com/openwrt-develop/luci-theme-atmaterial.git package/luci-theme-atmaterial
-git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
+# themes添加
+git clone --depth=1 https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom.git $LUCI_DIR/luci-theme-infinityfreedom
+git clone --depth=1 https://github.com/Leo-Jo-My/luci-theme-opentomcat.git $LUCI_DIR/luci-theme-opentomcat
+git clone --depth=1 https://github.com/openwrt-develop/luci-theme-atmaterial.git $LUCI_DIR/luci-theme-atmaterial
+# luci-theme-argon
+git clone --depth=1 https://github.com/jerrykuku/luci-theme-argon.git $LUCI_DIR/luci-theme-argon
+git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config $LUCI_DIR/luci-app-argon-config
 
-#git clone https://github.com/kiddin9/openwrt-packages.git package/openwrt-packages
+#git clone --depth=1 https://github.com/kiddin9/openwrt-packages.git package/openwrt-packages
 
-git clone https://github.com/sirpdboy/luci-app-netdata.git package/luci-app-netdata
+git clone --depth=1 https://github.com/sirpdboy/luci-app-netdata.git $LUCI_DIR/luci-app-netdata
 
-#添加额外非必须软件包
-git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome
-git clone https://github.com/vernesong/OpenClash.git package/OpenClash
-#git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
-git clone https://github.com/zzsj0928/luci-app-pushbot.git package/luci-app-pushbot
-git clone https://github.com/riverscn/openwrt-iptvhelper.git package/openwrt-iptvhelper
-#git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/luci-app-jd-dailybonus
-#添加smartdns
-git clone https://github.com/kiddin9/luci-app-dnsfilter.git package/luci-app-dnsfilter
-
-git clone https://github.com/pymumu/openwrt-smartdns package/smartdns
-git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/luci-app-smartdns
-# Add luci-theme-argon
-#git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon
-#git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config
-#rm -rf ../lean/luci-theme-argon
+# 添加额外非必须软件包
+git clone --depth=1 https://github.com/rufengsuixing/luci-app-adguardhome.git $LUCI_DIR/luci-app-adguardhome
+svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash $LUCI_DIR/luci-app-openclash
+#git clone --depth=1 https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
+git clone --depth=1 https://github.com/zzsj0928/luci-app-pushbot.git $LUCI_DIR/luci-app-pushbot
+git clone --depth=1 https://github.com/riverscn/openwrt-iptvhelper.git $LUCI_DIR/openwrt-iptvhelper
+#git clone --depth=1 https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/luci-app-jd-dailybonus
+git clone --depth=1 https://github.com/kiddin9/luci-app-dnsfilter.git $LUCI_DIR/luci-app-dnsfilter
+# 添加smartdns
+rm -rf feeds/luci/applications/luci-app-smartdns
+git clone --depth=1 https://github.com/pymumu/openwrt-smartdns $PACK_DIR/smartdns
+git clone --depth=1 -b lede https://github.com/pymumu/luci-app-smartdns $LUCI_DIR/luci-app-smartdns
+# 网络共享
+svn co https://github.com/coolsnowwolf/luci/trunk/applications/luci-app-cifsd $LUCI_DIR/luci-app-cifsd
+# aliyundrive-webdav
+svn co https://github.com/coolsnowwolf/luci/trunk/applications/luci-app-aliyundrive-webdav $LUCI_DIR/luci-app-aliyundrive-webdav
+svn co https://github.com/coolsnowwolf/packages/trunk/multimedia/aliyundrive-webdav $PACK_DIR/aliyundrive-webdav
